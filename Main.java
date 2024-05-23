a775805296475370209fba30d33cdf3617fddbe4
PolCatalin <151838347+PolCatalin@users.noreply.github.com>
2024-05-23 16:35:29 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        cadires cadira1 = new cadires("vermell", 4);
+        cadira1.taules("blau", 2);
+
+        taules taula1 = new taules("marró", "gran");
+        taula1.setColor("blanc");
+        taula1.setMida("petit");
+        System.out.println(taula1 + ) ;
+    }
+}
